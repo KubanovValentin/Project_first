0b55d4216a41cb2df7e72dc7580d88cd2d8bcefa
Valentin <valantinekubanov@yandex.by>
2022-05-25 20:01:22 +0300

    Add files via upload

--- Project_first/main.cpp
+++ Project_first/main.cpp
@@ -1,19 +1,13 @@
-#include <iostream>
-
+#include<iostream>
+#include<string>
 using namespace std;
-
-int main()
-{
-int sum;
-int p,s,k,x;
-cin >> sum;
-sum==p+s+k;
-p==s;
-k==(s+p)*2;
-x=p;
-sum==x+x+((x+x)*2);
-x=sum/6;
-
-cout <<x<<" "<<(x+x)*2<<" "<<x;
-    return 0;
+main(){
+int d,n, c[26]={2,7,1,8,2,8,1,8,2,8,4,5,9,0,4,5,2,3,5,3,6,0,2,8,7,5};
+cin>>n;
+if (n!=0) {
+for (d=0; d<=n; d++) {
+if (d==1) cout<<".";
+if (d==n&&c[n+1]>=5&&n!=25) c[d]=c[d]+1;
+cout<<c[d]; } }
+else cout<<"3";
 }
