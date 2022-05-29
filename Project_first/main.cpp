a5e446b7902e4498f7010ce4c8facba2dc8844ce
Valentin <valantinekubanov@yandex.by>
2022-05-29 15:09:26 +0300

    Add files via upload

--- Project_first/main.cpp
+++ Project_first/main.cpp
@@ -2,23 +2,7 @@
 using namespace std;
 main()
 {
-
-    int X1, Y1, X2 ,Y2;
-    cin >> X1 >> Y1;
-    cin >> X2 >> Y2;
-    if (X1 == X2 || Y1 == Y2)
-        cout << "YES";
-    else
-        cout << "NO";
-    return 0;
-
-
-
-    /*
-    int X1,Y1,X2,Y2;
-    cin>>X1>>Y1>>X2>>Y2;
-    int d=(Y2 - Y1) + (X2 - X1);
-   cout<<d;
-   */
+int X1,Y1,X2,Y2;
+cin>>X1>>Y1>>X2>>Y2;
+cout<<(X1==X2||Y1==Y2||X1-X2==Y1-Y2||X2-X1==Y2-Y1||-(X1-X2)==Y1-Y2||-(X2-X1)==Y2-Y1?"YES":"NO");
 }
-//l = y 2 - y 1 + x 2 - x 1
